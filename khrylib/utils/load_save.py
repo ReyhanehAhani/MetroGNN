1201b32129a583c56b8c045e9b48a692cb82755b
Reyhaneh Ahani <88882191+ReyhanehAhani@users.noreply.github.com>
2024-09-18 12:20:38 +0330

    Update load_save.py

--- khrylib/utils/load_save.py
+++ khrylib/utils/load_save.py
@@ -4,24 +4,11 @@ import pickle
 import os
 import socket
 
-
 def get_file_path(file_path):
-    hostname = socket.gethostname()
-    cwd = os.getcwd()
-    if hostname == 'LAPTOP-V8NBPG91':
-        file_path = os.path.join(cwd, file_path)
-    elif hostname == 'rl2':
-        file_path = os.path.join('/data1/suhongyuan/road_planning/code', file_path)
-    elif hostname == 'rl3':
-        file_path = os.path.join('/home/zhengyu/workspace/urban_planning', file_path)
-    elif hostname == 'rl4':
-        file_path = os.path.join('/home/zhengyu/workspace/urban_planning', file_path)
-    elif hostname == 'DL4':
-        file_path = os.path.join('/data2/zhengyu/workspace/urban_planning', file_path)
-    else:
-        raise ValueError('Unknown hostname: {}'.format(socket.gethostname()))
-    return file_path
-
+    # Bypassing hostname check and returning the original file path
+    cwd = os.getcwd()  # Keeping the current working directory
+    print(f"Using file path: {file_path}")
+    return os.path.join(cwd, file_path)  # Join the provided file path with the current working directory
 
 def load_yaml(file_path):
     file_path = get_file_path(file_path)
@@ -30,7 +17,6 @@ def load_yaml(file_path):
     assert(len(files) == 1)
     cfg = yaml.safe_load(open(files[0], 'r'))
     return cfg
-
 
 def load_pickle(file_path):
     file_path = get_file_path(file_path)
